96fdaddea423651dc35278b971a720772b07026e
laborflowusa <laborflowusa@gmail.com>
2026-03-12 18:41:38 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Disclaimer from './Disclaimer';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+function Root() {
+  const [accepted, setAccepted] = React.useState(false);
+  if (!accepted) return <Disclaimer onAccept={() => setAccepted(true)} />;
+  return <App />;
+    }
