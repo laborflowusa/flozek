add9249355266160a1c8b8d74893eb0c2dd32cb6
laborflowusa <laborflowusa@gmail.com>
2026-04-16 14:14:23 -0400

    add service worker fix

--- src/index.js
+++ src/index.js
@@ -11,3 +11,10 @@ function Root() {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Root />);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(() => console.log("SW registered"))
+      .catch((err) => console.log("SW failed", err));
+  });
+}
